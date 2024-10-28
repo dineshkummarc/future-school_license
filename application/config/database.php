13b429bdde3c566f33404a444b23da011008ef78
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-28 13:39:31 +0530

    update

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $active_record = TRUE;
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = 'just';
+$db['default']['database'] = 'justfutureschool';
 $db['default']['dbdriver'] = 'mysqli';
 
 
